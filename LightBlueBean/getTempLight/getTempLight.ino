282d7ca1c7c6f3ca630f87c9582ebb77d3447bd8
padailey <peteradailey@gmail.com>
2016-01-29 05:46:15 -0800

    Temperature Sensor now functioning correctly.
    Added initialization line in the setup() loop. Also removed extraneous
    comments.

--- LightBlueBean/getTempLight/getTempLight.ino
+++ LightBlueBean/getTempLight/getTempLight.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_TSL2561_U.h>
-#include "Adafruit_MCP9808.h" // I2C Temperature Sensor
+#include <Adafruit_TSL2561_U.h> // I2C Light Sensor
+#include "Adafruit_MCP9808.h" // I2C Temperature Sensor (default I2C address of 0x18)
 
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1);
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
@@ -11,50 +11,53 @@ Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 void configureSensor(void)
 {
   /* You can also manually set the gain or enable auto-gain support */
-  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
-  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
   tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
   
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 }
 
 
 void setup(){
   Serial.begin();
-  
+
+  // Rename the bean for node-red sketch
   String beanName = "Bean1";
   Bean.setBeanName(beanName);
   
-  /* Setup the sensor gain and integration time */
+  
+  /* Setup the LUX sensor gain and integration time */
   configureSensor();
+  
+  /* Setup the Temperature sensor */
+  tempsensor.begin();
 }
  
 void loop(){
   // Wait a moment for capacitance effects to settle
   delay(1000);
-  
-  /* Get a new sensor event */ 
+
+  //// LIGHT SENSOR /////
+  // Get a new sensor event
   sensors_event_t event;
   tsl.getEvent(&event);
  
   /* Display the results (light is measured in lux) */
-   /* If event.light = 0 lux the sensor is possibly saturated
-       and no reliable data could be generated! */
   Serial.print(event.light); 
   Serial.print(" lux, ");
  
-  
+
+  //// TEMPERATURE SENSOR /////
   // Get the current ambient temperature in degrees Celsius with a range of -40 C to 87 C.
-   tempsensor.shutdown_wake(0);   // Don't remove this line! required before reading temp
+  tempsensor.shutdown_wake(0);   // Don't remove this line! required before reading temp
   float c = tempsensor.readTempC(); // Read and print out the temperature
-  //Serial.print(stringToPrint);
   Serial.print(c);
   Serial.print(" C, ");
-  
+  tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere
+
+  //// VOLATAGE SENSOR /////
   // Returns the voltage with conversion of 0.01 V/unit
+  // Used to determine if sensor valeus above are valid
   uint16_t batteryReading =  Bean.getBatteryVoltage(); 
   // Format the output like "Battery voltage: 2.60 V"
   Serial.print(batteryReading/100);
