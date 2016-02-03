9f73ab3f3baebceb57a210b31ed483387957e7f5
padailey <peteradailey@gmail.com>
2016-02-02 20:36:40 -0800

    Saving Power by changing connection state. Also connected moisture sensor to digital output pin.
    Configuring beans to wake on connection.

--- LightBlueBean/getMoisture/getMoisture.ino
+++ LightBlueBean/getMoisture/getMoisture.ino
@@ -2,63 +2,66 @@
 #include <Wire.h>
 
 I2CSoilMoistureSensor sensor; // I2C Soil Sensor
+static uint8_t powerPin = 5; // will be used to power soil sensor. In series with 470 Ohm Resistor
+
 
 void setup() {
-  // Configure Bean to wake up when a client connects
-  Bean.enableWakeOnConnect(true);
-  
+    // Rename the bean for node-red sketch
+  String beanName = "Bean2";
+  Bean.setBeanName(beanName);
+
   Wire.begin();
   Serial.begin();
 
-  // Rename the bean for node-red sketch
-  String beanName = "Bean2";
-  Bean.setBeanName(beanName);
+  pinMode(powerPin, OUTPUT);
   
   // Start Soil Sensor
   sensor.begin(); // reset sensor
+  
+  // Configure Bean to wake up when a client connects
+  Bean.enableWakeOnConnect(true);
 }
 
+int counter = 0;
 
 // Loop runs once at program start.
 // When a client connects, Bean wakes up and runs loop again.
 // Loop continues to run as long as a client is connected.
 void loop(){
   bool connected = Bean.getConnectionState();
-  // Wait a moment for capacitance effects to settle
-  if (connected) {
-  delay(1000);
+  // Check if client has disconnected
+  if (connected && counter == 0) {
+    digitalWrite(powerPin, HIGH);
+    
+    delay(1000); // Wait a moment for capacitance effects to settle
 
-  //// SOIL SENSOR /////
-  // Returns the moisture level of the soil by capacitance
-  Serial.print(sensor.getCapacitance()); //read capacitance register
-  Serial.print(" Soil_Moisture_Capacitance, ");
+    //// SOIL SENSOR /////
+    // Returns the moisture level of the soil by capacitance
+    Serial.print(sensor.getCapacitance()); //read capacitance register
+    Serial.print(" Soil_Moisture_Capacitance, ");
 
-  //// TEMPERATURE SENSOR /////
-  Serial.print(sensor.getTemperature()/(float)10); //temperature register
-  Serial.print(" C, ");
+    //// TEMPERATURE SENSOR /////
+    Serial.print(sensor.getTemperature()/(float)10); //temperature register
+    Serial.print(" C, ");
   
-  //// VOLATAGE SENSOR /////
-  // Returns the voltage with conversion of 0.01 V/unit
-  // Used to determine if sensor valeus above are valid
-  uint16_t batteryReading =  Bean.getBatteryVoltage(); 
-  // Format the output like "Battery voltage: 2.60 V"
-  Serial.print(batteryReading/100);
-  Serial.print(".");
-  Serial.print(batteryReading%100);
-  Serial.print(" V");
+    //// VOLATAGE SENSOR /////
+    // Returns the voltage with conversion of 0.01 V/unit
+    // Used to determine if sensor valeus above are valid
+    uint16_t batteryReading =  Bean.getBatteryVoltage(); 
+    // Format the output like "Battery voltage: 2.60 V"
+    Serial.print(batteryReading/100);
+    Serial.print(".");
+    Serial.print(batteryReading%100);
+    Serial.print(" V");
   
-  Serial.print("\n"); //necessary for node-red to recognize that this is the end of input
-  //Bean.sleep(900000); // 15 min
-  //Bean.sleep(10000);
-
+    Serial.print("\n"); //necessary for node-red to recognize that this is the end of input
+    digitalWrite(powerPin, LOW);
+    counter = 1;
   } else {
     // Client disconnected
-    // This sketch runs only when a client is connected.
-    // This might be useful for interactive installations, where you don't want
-    // Bean to spend any power unless someone is actively connected.
-
     // Sleep for a long time to conserve power.
     // 0xFFFFFFFF = 4,294,967,295 milliseconds = 49 days
+    counter = 0;
     Bean.sleep(0xFFFFFFFF);
   }
 }
--- LightBlueBean/getWaterLevel/getWaterLevel.ino
+++ LightBlueBean/getWaterLevel/getWaterLevel.ino
@@ -1,18 +1,78 @@
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
-#include "Adafruit_LEDBackpack.h"
-#include "Adafruit_GFX.h"
 
-Adafruit_7segment disp = Adafruit_7segment(); // Sets up the 7-segment display
+//#include "Adafruit_LEDBackpack.h"  // Handles the display
+//#include "Adafruit_GFX.h" 
+
+#include <PinChangeInt.h> // Handles pin change interrupts
+
+//Adafruit_7segment disp = Adafruit_7segment(); // Sets up the 7-segment display
+
+// set the interrupt pin at D4
+int interruptPin = 4;
 
 void setup() {
-  disp.begin(0x70); // Initialize the 7-segment display
+  // Rename the bean for node-red sketch
+  String beanName = "BeanX";
+  Bean.setBeanName(beanName);
+  
+  //disp.begin(0x70); // Initialize the 7-segment display
+
+  pinMode(interruptPin, INPUT_PULLUP);
+   // run pinChanged when something changes on D0 
+   // This is an external library, so you don't need the prefix "Bean"
+   attachPinChangeInterrupt(interruptPin, pinChanged, CHANGE);
+
+  // Configure Bean to wake up when a client connects
+  Bean.enableWakeOnConnect(true);
 }
 
+int counter = 0;
+
+
+// Loop runs once at program start.
+// When a client connects, Bean wakes up and runs loop again.
+// Loop continues to run as long as a client is connected.
 void loop() {
-  // print a floating point 
-  disp.print(25);
-  disp.writeDisplay();
-  delay(1000);
-  disp.writeDisplay();
-  Bean.sleep(10000);
+  // Check if client has disconnected
+  bool connected = Bean.getConnectionState();
+
+  // Sleep all the time when not handling an interrupt
+  if (connected && counter == 0) {
+    int c = Bean.getTemperature();
+    Serial.print(c);
+    Serial.print(" C, ");
+    Bean.setLed(0, 255, 0);
+    
+    //// VOLATAGE SENSOR /////
+    // Returns the voltage with conversion of 0.01 V/unit
+    // Used to determine if sensor valeus above are valid
+    uint16_t batteryReading =  Bean.getBatteryVoltage(); 
+    // Format the output like "Battery voltage: 2.60 V"
+    Serial.print(batteryReading/100);
+    Serial.print(".");
+    Serial.print(batteryReading%100);
+    Serial.print(" V");
+  
+    Serial.print("\n"); //necessary for node-red to recognize that this is the end of input
+    counter = 1;
+  } else {
+    Bean.setLed(0, 0, 0);
+    // Client disconnected: 
+    // Sleep for a long time to conserve power.
+    // 0xFFFFFFFF = 4,294,967,295 milliseconds = 49 days
+    counter = 0;
+    Bean.sleep(0xFFFFFFFF);
+  }
+}
+
+
+// Interrupt service routine (ISR) needs to return void and accept no arguments
+void pinChanged() {
+  // print a floating point
+  //disp.print(25);
+  //disp.writeDisplay();
+  Serial.print("Button Pressed");
+  //disp.writeDigitRaw(1,B00000001);
+  // will display nothing
+  //disp.writeDisplay();
 }
