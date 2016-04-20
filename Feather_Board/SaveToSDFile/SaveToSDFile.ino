fa9e913513053ae8d8f23a6f01e103680de69444
padailey <peteradailey@gmail.com>
2016-04-20 15:22:14 -0700

    Refactoring
    - Removed commented code.
    - Revised comments
    - Created function writeDataToSD()

--- Feather_Board/SaveToSDFile/SaveToSDFile.ino
+++ Feather_Board/SaveToSDFile/SaveToSDFile.ino
@@ -7,18 +7,22 @@ String base_name = "log_";
 String file_name;
 const int MAX_FILES = 100;
 
-// Serial Variables
+// Serial Communication Variables
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
-
-// TODO
-boolean written_already = false;
+// Deafult Baudrates for Sensors
+const double IMU_BAUD = 115200;
+//const double GPS_BAUD = 57600;
 
 void setup() {
-  /*=== USB and Board Serial===*/
-  Serial.begin(9600);           // USB Serial
-  Serial1.begin(115200);        // Board Serial
+  /*=== USB and Board Serial===
+   * If the Serial1 baud is not set correctly, the board will not 
+   * recognize the newline character, and data will not be returned.
+   */
+  Serial.begin(115200);         // USB
+  //Serial1.begin(GPS_BAUD);      // GPS
+  Serial1.begin(IMU_BAUD);    // IMU
 
   // wait for USB serial port to connect.
   while (!Serial) {
@@ -38,19 +42,18 @@ void setup() {
 
 
   /*=== Data Files ===
-      Select a fileneame to save data to. Look for files already on the SD card to prevent naming collisions.
-  */
+   * Select a fileneame to save data to. Look for files already on the SD card to prevent naming collisions.
+   */
   for (int i = 1; i < MAX_FILES; i++) {
     file_name = base_name + i + ".csv";
 
-    if (SD.exists(file_name)) {
-      Serial.print(file_name);
-      Serial.println(" exists.");
-    } else {
+    if (SD.exists(file_name)) { //check if file name already exists
+      //Serial.print(file_name);
+      //Serial.println(" exists.");
+    } else {  // write to file
       file = SD.open(file_name, FILE_WRITE);
       Serial.print("Saving to ");
       Serial.println(file_name);
-      //TODO: will need to open and close
       file.close();
       break;
     }
@@ -58,57 +61,48 @@ void setup() {
 }
 
 void loop() {
-  if (written_already == false) {
-    inputString = "I have a big mouth!";
+  /*=== Retrieve Data from Serial1 ===
+   * Print the string when a newline arrives
+   */
+  if (stringComplete) {
+    //Serial.println(inputString);
+    writeDataToSD();
 
-    // Open file for writing
-    file = SD.open(file_name, FILE_WRITE);
-    if (file) {
-
-      Serial.print("Writing to ");
-      Serial.print(file_name);
-
-      file.println("testing 1, 2, 3.");
-      file.println(inputString);
-
-      // close the file
-      file.close();
-      Serial.println(" ...done.");
-    } else {
-      // if the file didn't open, print an error:
-      Serial.println("Error opening test.txt");
-    }
-
-    written_already = true;
-  }
-
-  /*=== Write Data to SD Card ===*/
-
-
-  /*=== Revieve Data from Serial1 ===*/
-  /*
-     // print the string when a newline arrives:
-    if (stringComplete) {
-    Serial.println(inputString);
-    // clear the string:
+    // Clear the string
     inputString = "";
     stringComplete = false;
-    }
+  }
 
-    while (Serial1.available()) {
-    // get the new byte:
+  /*=== Retrieve sentence from Serial1 === 
+  */
+  while (Serial1.available()) {
+    // Get the new byte and add it to the input string
     char inChar = (char)Serial1.read();
-    //Serial.println(inChar);
-
-    // add it to the inputString:
     inputString += inChar;
+    
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
       stringComplete = true;
     }
-    }
-  */
+  }
+}
 
+
+
+/*=== Write Data to SD Card ===*/
+void writeDataToSD() {
+  Serial.print(inputString);
+
+  // Open file for writing
+  file = SD.open(file_name, FILE_WRITE);
+
+  // if file sucessfully opened
+  if (file) {
+    file.print(inputString);
+    file.close();
+  } else {
+    Serial.println("Error opening file");
+  }
 }
 
