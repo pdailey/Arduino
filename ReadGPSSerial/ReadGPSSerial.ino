7d1b27e962eaec10b8bbdd80a10ba13d16270437
padailey <peteradailey@gmail.com>
2016-04-20 10:05:51 -0700

    Playing with 32u4 Microprocessor

--- ReadGPSSerial/ReadGPSSerial.ino
+++ ReadGPSSerial/ReadGPSSerial.ino
@@ -1,6 +1,7 @@
 /*
-  Serial Event example
-
+  Serial Event example using 32u4 microprocesor
+ https://www.arduino.cc/en/Guide/ArduinoLeonardoMicro
+ 
  When new serial data arrives, this sketch adds it to a String.
  When a newline is received, the loop prints the string and
  clears it.
@@ -8,13 +9,24 @@
  A good test for this is to try it with a GPS receiver
  that sends out NMEA 0183 sentences.
  */
+ 
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 4;
+
 
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
 void setup() {
   // initialize serial:
-  Serial.begin(115200);
+  Serial1.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. DO NOT REMOVE!
+  }
+  
+  
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
 }
@@ -27,18 +39,12 @@ void loop() {
     inputString = "";
     stringComplete = false;
   }
-}
-
-/*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
-void serialEvent() {
-  while (Serial.available()) {
+  
+  while (Serial1.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read();
+    char inChar = (char)Serial1.read();
+    //Serial.println(inChar);
+    
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag
@@ -47,6 +53,27 @@ void serialEvent() {
       stringComplete = true;
     }
   }
+
 }
 
-
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+ // SerialEvent does not work on 32u4 boards!
+/*void serialEvent() {
+  while (Serial1.available()) {
+    Serial1.print("SerialEvent!");
+    // get the new byte:
+    char inChar = (char)Serial1.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}*/
