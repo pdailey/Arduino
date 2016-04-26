47247e06518a5c83a7a17a9948920591281647e5
padailey <peteradailey@gmail.com>
2016-04-26 15:59:50 -0700

    refactoring

--- Feather_Board/SaveToSDFile/SaveToSDFile.ino
+++ Feather_Board/SaveToSDFile/SaveToSDFile.ino
@@ -7,7 +7,7 @@
 File file;
 String base_name = "log_";
 String file_name;
-const int MAX_FILES = 100;
+const int MAX_FILES = 1000;
 
 // Serial Communication Variables
 String inputString = "";         // a string to hold incoming data
@@ -15,7 +15,7 @@ boolean stringComplete = false;  // whether the string is complete
 
 // Deafult Baudrates for Sensors
 const double IMU_BAUD = 115200;
-//const double GPS_BAUD = 57600;
+const double GPS_BAUD = 57600;
 
 
 void setup() {  
@@ -23,17 +23,18 @@ void setup() {
    * If the Serial1 baud is not set correctly, the board will not 
    * recognize the newline character, and data will not be returned.
    */
-  Serial.begin(115200);         // USB
-  //Serial1.begin(GPS_BAUD);      // GPS
-  Serial1.begin(IMU_BAUD);    // IMU
+  Serial.begin(IMU_BAUD);
+  
+  Serial1.begin(GPS_BAUD);
+  //Serial1.begin(IMU_BAUD);
 
   // wait for USB serial port to connect.
   while (!Serial) {
     ;
   }
 
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  // Reserve bytes for the inputString:
+  inputString.reserve(20000);
 
 
   /*=== SD Card ===*/
@@ -44,15 +45,14 @@ void setup() {
   Serial.println("SD initialized.");
 
 
-  /*=== Data Files ===
-   * Select a fileneame to save data to. Look for files already on the SD card to prevent naming collisions.
-   */
+  /*=== Data Files ===*/
+  // Select a fileneame to save data to. Look for files already on the SD card to prevent naming collisions.
   for (int i = 1; i < MAX_FILES; i++) {
     file_name = base_name + i + ".csv";
 
     if (SD.exists(file_name)) { //check if file name already exists
-      //Serial.print(file_name);
-      //Serial.println(" exists.");
+      Serial.print(file_name);
+      Serial.println(" exists.");
     } else {  // write to file
       file = SD.open(file_name, FILE_WRITE);
       Serial.print("Saving to ");
@@ -80,27 +80,27 @@ void loop() {
   */
   while (Serial1.available()) {
     // Get the new byte and add it to the input string
-    char inChar = (char)Serial1.read();
-    inputString += inChar;
+    char c = (char) Serial1.read();
+    inputString += c;
+    //Serial.print(c);
     
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
-    if (inChar == '\n') {
+    if (c == '\n') {
       stringComplete = true;
+      //Serial.println(inputString);
     }
   }
 }
-
 
 
 /*=== Write Data to SD Card ===*/
 void writeDataToSD() {
   Serial.print(inputString);
 
-  // Open file for writing
   file = SD.open(file_name, FILE_WRITE);
 
-  // if file sucessfully opened
+  // If file sucessfully opened
   if (file) {
     file.print(inputString);
     file.close();
