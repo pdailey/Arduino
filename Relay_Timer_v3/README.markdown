31802abdca5055c4b98a33a843869807ac88f7be
pdailey <peteradailey@gmail.com>
2016-09-17 15:53:26 -0400

    Solar Inverter

---
+++ Relay_Timer_v3/README.markdown
@@ -0,0 +1,10 @@
+# 4 Relay Timer /  Automaton
+
+Triggers a relay module for a set period of time. Built using the Automaton library.
+
+Relay starts automatically, alternating between running 2 fans for 15 min, and 2 heaters for 30 min.
+
+Pins and cycle times are configured within the sketch.
+
+Created by modifying this tutorial:
+https://github.com/tinkerspy/Automaton/wiki/Machine-building-tutorial-2#adding-a-push-connector
---
+++ Relay_Timer_v3/Relay_Timer_v3.ino
@@ -0,0 +1,47 @@
+/*
+ * 6 Relay Automaton
+ *
+ * Triggers a 6 relay module for a set period fo time using the Automaton library.
+ * Relay starts automatically, alternating between running 2 fans for 15 min, and 4 heaters for 30 min.
+ *
+ * Pins and cycle times can be configured within the sketch.
+ *
+ * Based off of this tutorial:
+ * https://github.com/tinkerspy/Automaton/wiki/Machine-building-tutorial-2#adding-a-push-connector
+ */
+
+#include <Automaton.h>
+#include "Atm_Relay.h"
+
+// Basic Arduino sketch - instantiates the state machine and nothing else
+Atm_Relay Relay;
+
+// Time that the cycles will be on. Can handle 4,294,967,294 ms or ~50 Days
+// time entered in ms
+const unsigned long min_cool = 3600000;
+const unsigned long min_heat = 900000;
+
+// Pins atached to the relay
+const byte heat_pin_1 = 8;
+const byte heat_pin_2 = 9;
+const byte heat_pin_3 = 10;
+const byte heat_pin_4 = 11;
+const byte heat_pin_5 = 12;
+const byte heat_pin_6 = 13;
+
+void setup() {
+  Serial.begin( 9600 );
+  // Display changes in state over serial
+  Relay.trace( Serial );
+
+  // Assign the pins to the relays
+  Relay.begin(heat_pin_1, heat_pin_2, heat_pin_3, heat_pin_4, heat_pin_5, heat_pin_6)
+    // Set the time of the cooling and heating cycle. Cooling cycle runs first
+    .automatic(min_cool, min_heat);
+}
+
+void loop() {
+  automaton.run();
+}
+
+
