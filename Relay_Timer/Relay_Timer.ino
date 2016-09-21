00dddaca07d1ffa95ba5120c9314e808ae102bd7
pdailey <peteradailey@gmail.com>
2016-09-20 21:23:46 -0400

    refactoring

--- Relay_Timer/Relay_Timer.ino
+++ Relay_Timer/Relay_Timer.ino
@@ -1,11 +1,11 @@
 /*
  * 4 Relay Automaton
- * 
- * Triggers a 4 relay module for a set period fo time using the Automaton library. 
- * Relay starts automatically, alternating between running 2 fans for 15 min, and 2 heaters for 30 min. 
- * 
+ *
+ * Triggers a 4 relay module for a set period of time using the Automaton library.
+ * Relay starts automatically, alternating between running 2 fans for 15 min, and 2 heaters for 30 min.
+ *
  * Pins and cycle times can be configured within the sketch.
- * 
+ *
  * Based off of this tutorial:
  * https://github.com/tinkerspy/Automaton/wiki/Machine-building-tutorial-2#adding-a-push-connector
  */
@@ -14,7 +14,7 @@
 #include "Atm_Relay.h"
 
 // Basic Arduino sketch - instantiates the state machine and nothing else
-Atm_Relay Relay;
+Atm_Relay relay;
 
 // Time that the cycles will be on. Can handle 4,294,967,294 ms or ~50 Days
 const unsigned long min_cool = 1800000; //1 * (30*1000);
@@ -28,13 +28,15 @@ const byte heat_pin_2 = 12;
 
 void setup() {
   Serial.begin( 9600 );
-  // Display changes in state over serial
-  Relay.trace( Serial );
 
-  // Assign the 4 pins to the relay
-  Relay.begin(cool_pin_1, cool_pin_2, heat_pin_1, heat_pin_2)
-    // Set the time of the cooling and heating cycle. Cooling cycle runs first
-    .automatic(min_cool, min_heat);
+  // Assign 4 pins to the relay
+  relay.begin(cool_pin_1, cool_pin_2, heat_pin_1, heat_pin_2)
+    // Display changes in state over serial
+    .trace( Serial )
+    // Set the time of the cooling and heating cycle.
+    .automatic(min_cool, min_heat)
+    // Start on a heating cycle, because that's typically when SHTF
+    .trigger(relay.EVT_HEAT);
 }
 
 void loop() {
